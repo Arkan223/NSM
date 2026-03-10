eeca59665d336542c4634ae264626178e8e60899
Arkan223 <76471453+Arkan223@users.noreply.github.com>
2026-03-11 00:37:11 +0300

    Update Firebase messaging service worker configuration

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,20 +2,27 @@ importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    databaseURL: "https://arkanlab-99711-default-rtdb.firebaseio.com/",
-    messagingSenderId: "871697042781"
+  apiKey: "AIzaSyApPJjEWPfz5njRS714IuleqJ6fzrroEZE",
+  authDomain: "arkanlab-99711.firebaseapp.com",
+  databaseURL: "https://arkanlab-99711-default-rtdb.firebaseio.com",
+  projectId: "arkanlab-99711",
+  storageBucket: "arkanlab-99711.firebasestorage.app",
+  messagingSenderId: "871697042781",
+  appId: "1:871697042781:web:74e6fbdd5f1196aeba2cb4",
+  measurementId: "G-JBC3R7SN7G"
 });
 
 const messaging = firebase.messaging();
 
-// معالجة الإشعارات عندما يكون المتصفح مغلقاً أو التطبيق في الخلفية
-messaging.onBackgroundMessage((payload) => {
-    const notificationTitle = payload.notification.title || "منظومة النخب";
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: 'https://cdn-icons-png.flaticon.com/512/1041/1041916.png',
-        badge: 'https://cdn-icons-png.flaticon.com/512/1041/1041916.png'
-    };
+messaging.onBackgroundMessage(function(payload) {
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
+  const notificationTitle = payload.notification?.title || "رسالة جديدة";
+  
+  const notificationOptions = {
+    body: payload.notification?.body || "",
+    icon: "https://cdn-icons-png.flaticon.com/512/1041/1041916.png"
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+
 });
